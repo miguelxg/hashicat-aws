4b2e583368f19911b0c3585c0098d2815a0c0abb
miguelxg <miguelxg@amazon.com>
2023-09-04 22:32:23 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "mgiuelxg-tf-isv-training"
+#   bucket = "mgiuelxg-tf-isv-training"
   bucket_prefix = "miguelgonzalez"
   acl    = "private"
 
