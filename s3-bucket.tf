37376d557cac3b1d04dc7c60743f5550519d6e6c
miguelxg <miguelxg@amazon.com>
2023-09-04 22:04:28 +0000

    created s3 bucket

---
+++ s3-bucket.tf
@@ -0,0 +1,13 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "mgiuelxg-tf-isv-training"
+  bucket_prefix = "miguelgonzalez"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+  
+}
